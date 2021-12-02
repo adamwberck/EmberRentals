04358f2a40d98aafa44e3f2c7f1e93e0097cc432
Adam W Berck <adamwberck@gmail.com>
2021-12-02 09:39:05 -0800

    loading button is working!

--- app/components/loading-button.js
+++ app/components/loading-button.js
@@ -2,9 +2,7 @@ import Component from '@ember/component';
 import { task, timeout } from 'ember-concurrency';
 
 export default Component.extend({
-    task:
-        (function *waitOneSecond(params) {
-            yield timeout(1000);
-        }
-    )
+    waitOneSecond: task(function* (action) {
+        yield timeout(1000);   
+    })
 });
